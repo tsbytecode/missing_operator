3c0b224eb7fcf9971ec24508544c7992392a5586
tsbytecode <tushar.sudeep@gmail.com>
2023-05-16 11:59:21 +0530

    Minor bug fixes and upgrade look.

--- MissingOperator.py
+++ MissingOperator.py
@@ -1,26 +1,25 @@
 import random
 def main_code():
-    question = []
-    operator = []
-    answer = ""
-    statement = ""
-    result = 0
-    difficulty = 0
+    operator = question = []
+    answer = statement = ""
+    difficulty = result = 0
     inptType = ""
+    def error():
+        print("\n----------Error in input. Please retry.----------\n\n==================================================\n")
+        main_code()
     def rand_num():
         for i in range(difficulty+1):
             question.append(random.randint(1, 9))
     def type_num():
         inptValues = []
-        print("Type \"exit\" when you are done adding inputs.")
+        print("Type \'exit\' when you are done adding inputs.")
         while 1>0:
             inptValues.append(input("Enter number : "))        
             if "exit" in inptValues:
                 inptValues.pop()
                 break
             if not(inptValues[-1].isdigit()):
-                print("\n----------Error in input. Please retry.----------\n")
-                main_code()
+                error()
         for i in range(len(inptValues)):
             question.append(int(inptValues[i]))
     def rand_sign():
@@ -36,21 +35,18 @@ def main_code():
         for j in range(difficulty):
             answer = answer + answerList[j]
         return answer
-    print("\nMissing Operator Game - Find the missing operator!\n")
-    inptType = input("Do you want to randomly generate numbers? (y/n) : ")
-    if inptType == "y":
+    inptType = input("Do you want to randomly generate numbers? (y/n) : ").lower()
+    if inptType.startswith("y"):
         difficulty = input("How many blanks do you want? (Difficulty) : ")
         if not(difficulty.isdigit()):
-            print("\n----------Error in input. Please retry.----------\n")
-            main_code()
+            error()
         difficulty = int(difficulty)
         rand_num()
-    elif inptType == "n":
+    elif inptType.startswith("n"):
         type_num()
         difficulty = len(question) - 1
     else:
-        print("\n----------Error in input. Please retry.----------\n")
-        main_code()
+        error()
     answer = rand_sign()
     for i in range(difficulty+1):
         result = result + question[i]
@@ -63,4 +59,6 @@ def main_code():
         print("Correct!")
     else:
         print("Oh no! Wrong answer. \nRight answer is",answer,"\n")
+        main_code()
+print("\n==================================================\n\nMissing Operator Game - Find the missing operator!\n")
 main_code()
